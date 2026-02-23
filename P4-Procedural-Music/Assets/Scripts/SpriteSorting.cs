ebcd6d389f8d4828619fa79c2c85b968b5293e84
MathiasPHA <Mathias.p.h.andersen@gmail.com>
2026-02-23 13:16:02 +0100

    Trees

---
+++ P4-Procedural-Music/Assets/Scripts/SpriteSorting.cs
@@ -0,0 +1,31 @@
+using UnityEngine;
+
+public class SpriteSorting : MonoBehaviour
+{
+    public bool isColliding = false;
+    void Start()
+    {
+        
+    }
+    void Update()
+    {
+        
+    }
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.CompareTag("Player") || collision.gameObject.CompareTag("Mob"))
+        {
+            isColliding = true;
+            //GetComponent<SpriteRenderer>().sortingOrder = 1;
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collision.gameObject.CompareTag("Player") || collision.gameObject.CompareTag("Mob"))
+        {
+            isColliding = false;
+            //GetComponent<SpriteRenderer>().sortingOrder = 0;
+        }
+    }
+}
