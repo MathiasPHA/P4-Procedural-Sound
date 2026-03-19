adff4e1746a44b9ed4c6f0501cc9c2ea2c6aaad3
GustavNH <gustavnoerhaugesen@gmail.com>
2026-03-19 09:49:32 +0100

    y

--- P4-Procedural-Music/Assets/Scripts/DayNight/SunRotation.cs
+++ P4-Procedural-Music/Assets/Scripts/DayNight/SunRotation.cs
@@ -12,30 +12,26 @@ public class SunRotation : MonoBehaviour
     private void Awake()
     {
         timeReference = GameObject.Find("Day Night System").GetComponent<TimeReference>();
-
-
     }
 
     void Start()
     {
         light = GetComponent<Light2D>(); // Get the Light2D component attached to the same GameObject
-
-
     }
 
     // Update is called once per frame
     void Update()
     {
 
+        if (lightIntensity <= 1.35f)
+        {
+            light.intensity = lightIntensity * (timeReference.time / 12f);
+        }
 
-        light.intensity = lightIntensity * (timeReference.time / 12f);
 
         if (lightIntensity >= 1.35f)
         {
-            for (float i = 0; i <= 0f; i *= -1)
-            {
-                lightIntensity = i;
-            }
+            light.intensity = -1 * lightIntensity * (timeReference.time / 12f);
         }
 
     }
