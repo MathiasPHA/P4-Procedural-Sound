4507a8c190aa0b59dd0a8528c462d2a228807e87
GustavNH <gustavnoerhaugesen@gmail.com>
2026-03-19 09:44:27 +0100

    hm

--- P4-Procedural-Music/Assets/Scripts/DayNight/SunRotation.cs
+++ P4-Procedural-Music/Assets/Scripts/DayNight/SunRotation.cs
@@ -19,7 +19,6 @@ public class SunRotation : MonoBehaviour
     void Start()
     {
         light = GetComponent<Light2D>(); // Get the Light2D component attached to the same GameObject
-        lightIntensity = light.intensity; // Get the initial intensity of the sun light and store it in lightIntensity
 
 
     }
@@ -27,29 +26,18 @@ public class SunRotation : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        Vector2 center = new Vector2(0, 0);
-        transform.position = GetRotatedPosition(timeReference.time, 10f, center);
 
 
-        light.intensity = lightIntensity * (timeReference.time / 24f);
+        light.intensity = lightIntensity * (timeReference.time / 12f);
 
         if (lightIntensity >= 1.35f)
         {
-            lightIntensity -= 1.35f;
+            for (float i = 0; i <= 0f; i *= -1)
+            {
+                lightIntensity = i;
+            }
         }
 
-    }
-
-    Vector2 GetRotatedPosition(float hour, float radius, Vector2 center)
-    {
-        // Map 0-24 to 0-360 degrees
-        float angle = (hour / 24f) * Mathf.PI * -2f;
-
-        // Offset by -pi/2 so 0 starts at the top (12 o'clock)
-        float x = center.x + Mathf.Cos(angle - Mathf.PI / 2f) * radius;
-        float y = center.y + Mathf.Sin(angle - Mathf.PI / 2f) * radius;
-
-        return new Vector2(x, y);
     }
 
 }
