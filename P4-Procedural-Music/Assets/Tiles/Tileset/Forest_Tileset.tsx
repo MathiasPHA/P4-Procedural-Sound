880d87cdd99dc2c951a7e3ddfc4bbab0e58cad57
MathiasPHA <Mathias.p.h.andersen@gmail.com>
2026-03-26 12:15:58 +0100

    Tileset

---
+++ P4-Procedural-Music/Assets/Tiles/Tileset/Forest_Tileset.tsx
@@ -0,0 +1,151 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.11.0" name="Forest_Tileset" tilewidth="2000" tileheight="2000" tilecount="49" columns="0">
+ <grid orientation="orthogonal" width="1" height="1"/>
+ <tile id="1">
+  <image source="Grass/Blank.png" width="2000" height="2000"/>
+ </tile>
+ <tile id="2">
+  <image source="Water/W_3T.png" width="2000" height="2000"/>
+ </tile>
+ <tile id="3">
+  <image source="Water/W_BendBL.png" width="2000" height="2000"/>
+ </tile>
+ <tile id="4">
+  <image source="Water/W_BendBR.png" width="2000" height="2000"/>
+ </tile>
+ <tile id="5">
+  <image source="Water/W_BendTL.png" width="2000" height="2000"/>
+ </tile>
+ <tile id="6">
+  <image source="Water/W_BendTR.png" width="2000" height="2000"/>
+ </tile>
+ <tile id="7">
+  <image source="Water/W_BL.png" width="2000" height="2000"/>
+ </tile>
+ <tile id="8">
+  <image source="Water/W_BM.png" width="2000" height="2000"/>
+ </tile>
+ <tile id="9">
+  <image source="Water/W_BR.png" width="2000" height="2000"/>
+ </tile>
+ <tile id="10">
+  <image source="Water/W_EB.png" width="2000" height="2000"/>
+ </tile>
+ <tile id="11">
+  <image source="Water/W_EL.png" width="2000" height="2000"/>
+ </tile>
+ <tile id="12">
+  <image source="Water/W_ER.png" width="2000" height="2000"/>
+ </tile>
+ <tile id="13">
+  <image source="Water/W_ET.png" width="2000" height="2000"/>
+ </tile>
+ <tile id="15">
+  <image source="Water/W_Mid.png" width="2000" height="2000"/>
+ </tile>
+ <tile id="17">
+  <image source="Water/W_3B.png" width="2000" height="2000"/>
+ </tile>
+ <tile id="18">
+  <image source="Water/W_3L.png" width="2000" height="2000"/>
+ </tile>
+ <tile id="19">
+  <image source="Water/W_3R.png" width="2000" height="2000"/>
+ </tile>
+ <tile id="20">
+  <image source="Water/W_Single.png" width="2000" height="2000"/>
+ </tile>
+ <tile id="21">
+  <image source="Water/W_TL.png" width="2000" height="2000"/>
+ </tile>
+ <tile id="22">
+  <image source="Water/W_TM.png" width="2000" height="2000"/>
+ </tile>
+ <tile id="23">
+  <image source="Water/W_TR.png" width="2000" height="2000"/>
+ </tile>
+ <tile id="24">
+  <image source="Water/W_X.png" width="2000" height="2000"/>
+ </tile>
+ <tile id="25">
+  <image source="Water/W_SingleLR.png" width="2000" height="2000"/>
+ </tile>
+ <tile id="26">
+  <image source="Water/W_SingleUD.png" width="2000" height="2000"/>
+ </tile>
+ <tile id="43">
+  <image source="Water/W_MOD.png" width="2000" height="2000"/>
+ </tile>
+ <tile id="44">
+  <image source="Water/W_MOL.png" width="2000" height="2000"/>
+ </tile>
+ <tile id="45">
+  <image source="Water/W_MOR.png" width="2000" height="2000"/>
+ </tile>
+ <tile id="46">
+  <image source="Water/W_MOT.png" width="2000" height="2000"/>
+ </tile>
+ <tile id="47">
+  <image source="Water/W_2C1.png" width="2000" height="2000"/>
+ </tile>
+ <tile id="48">
+  <image source="Water/W_2C2.png" width="2000" height="2000"/>
+ </tile>
+ <tile id="49">
+  <image source="Grass/G_Mid.png" width="2000" height="2000"/>
+ </tile>
+ <tile id="50">
+  <image source="Water/W_LM.png" width="2000" height="2000"/>
+ </tile>
+ <tile id="51">
+  <image source="Water/W_RM.png" width="2000" height="2000"/>
+ </tile>
+ <tile id="52">
+  <image source="Water/W_ICTL.png" width="2000" height="2000"/>
+ </tile>
+ <tile id="53">
+  <image source="Water/W_ICTR.png" width="2000" height="2000"/>
+ </tile>
+ <tile id="54">
+  <image source="Water/W_ICBL.png" width="2000" height="2000"/>
+ </tile>
+ <tile id="55">
+  <image source="Water/W_ICBR.png" width="2000" height="2000"/>
+ </tile>
+ <tile id="56">
+  <image source="Water/W_2OBL.png" width="2000" height="2000"/>
+ </tile>
+ <tile id="57">
+  <image source="Water/W_2OBR.png" width="2000" height="2000"/>
+ </tile>
+ <tile id="58">
+  <image source="Water/W_2OTL.png" width="2000" height="2000"/>
+ </tile>
+ <tile id="59">
+  <image source="Water/W_2OTR.png" width="2000" height="2000"/>
+ </tile>
+ <tile id="64">
+  <image source="Water/W_UCL.png" width="2000" height="2000"/>
+ </tile>
+ <tile id="65">
+  <image source="Water/W_UCR.png" width="2000" height="2000"/>
+ </tile>
+ <tile id="66">
+  <image source="Water/W_DCL.png" width="2000" height="2000"/>
+ </tile>
+ <tile id="67">
+  <image source="Water/W_DCR.png" width="2000" height="2000"/>
+ </tile>
+ <tile id="68">
+  <image source="Water/W_LCD.png" width="2000" height="2000"/>
+ </tile>
+ <tile id="69">
+  <image source="Water/W_LCU.png" width="2000" height="2000"/>
+ </tile>
+ <tile id="70">
+  <image source="Water/W_RCD.png" width="2000" height="2000"/>
+ </tile>
+ <tile id="71">
+  <image source="Water/W_RCU.png" width="2000" height="2000"/>
+ </tile>
+</tileset>
