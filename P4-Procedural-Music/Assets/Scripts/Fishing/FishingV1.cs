22b99db9b814d8af5c56fcefb48ee360fe67688e
GustavNH <gustavnoerhaugesen@gmail.com>
2026-04-14 14:54:46 +0200

    uhm

--- P4-Procedural-Music/Assets/Scripts/Fishing/FishingV1.cs
+++ P4-Procedural-Music/Assets/Scripts/Fishing/FishingV1.cs
@@ -2,6 +2,16 @@ using UnityEngine;
 
 public class FishingV1 : MonoBehaviour
 {
+
+    public float FishingTime = 10f;
+
+    [SerializeField] private Transform Fish;
+    [SerializeField] private Transform startFishPos;
+    [SerializeField] private float fishClamp;
+    [SerializeField] private float fishDestination;
+    [SerializeField] private float fishPos;
+    [SerializeField] private float swimSpeed = 1f;
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
@@ -11,6 +21,17 @@ public class FishingV1 : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        if (FishingTime > 0)
+        {
+            FishingTime -= Time.deltaTime;
+            Fishing();
+        }
     }
+
+    private void Fishing()
+    {
+        
+
+    }
+
 }
